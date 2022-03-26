a63404ff9202c3fbb7434afc7bc21f66d303ad16
Manuel Bugallo <marvinody@gmail.com>
2022-03-26 19:37:57 -0400

    adds sequelize config

---
+++ config/config.js
@@ -0,0 +1,26 @@
+// Load environment variable from .env file
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD || null,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD || null,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD || null,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    dialect: 'postgres'
+  }
+}
